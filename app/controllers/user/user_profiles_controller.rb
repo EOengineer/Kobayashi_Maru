8c91354a76f3c1b84ca36a2185e638c1cb1cbf75
Eric Oligney <EOengineer@gmail.com>
2014-12-13 22:40:52 -0500

    modified user profiles form.

--- app/controllers/user/user_profiles_controller.rb
+++ app/controllers/user/user_profiles_controller.rb
@@ -1,4 +1,5 @@
 class User::UserProfilesController < ApplicationController
+  before_action :authenticate_user!
 
   def new
     @user_profile = UserProfile.new
--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -1,3 +1,11 @@
 class UserProfile < ActiveRecord::Base
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :company, presence: true
+  validates :about, presence: true
+  validates :mission, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :contact, presence: true
 end
